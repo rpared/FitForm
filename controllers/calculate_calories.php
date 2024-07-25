e2e155126a46413ec33bbcbb10d6dad56cb2f146
roger <paredes.roger@gmail.com>
2024-07-25 15:20:33 -0400

    Creates users, profiles and statistics, also fetches them accordingly into the user home. Calculates cals too. Editing views and process pending as well as macro calsulators.

--- controllers/calculate_calories.php
+++ controllers/calculate_calories.php
@@ -1,5 +1,4 @@
 <?php
-
 // VALIDATORS 
 $age = $_POST['age'];
 $gender = $_POST['gender'];
@@ -28,15 +27,27 @@ if (!is_numeric($weight) || $weight <= 0) {
 }
 
 // Validate activity level
-if (!in_array($activity, ["1.2", "1.375", "1.55", "1.725", "1.9"])) {
+$activity_levels = ["1.2", "1.375", "1.55", "1.725", "1.9"];
+if (!in_array($activity, $activity_levels)) {
     die("Invalid activity level provided.");
 }
 
-/* Here we will perform calculations and return values for several objectives:
-    BMI, existing
-    Weight loss
-    Maintenance
-    Weight gain
+// If all inputs are valid, instantiate the CalorieCalculator class
+require_once '../models/Calorie_calculator_class.php';
 
-*/
+$calculator = new CalorieCalculator($age, $gender, $height, $weight, $activity);
+$calories = $calculator->calculateCalories();
+
+$bmr = $calculator->calculateBMR();
+$caloriesForMaintenance = $calculator->caloriesForMaintenance();
+$caloriesForWeightLoss = $calculator->caloriesForWeightLoss();
+$caloriesForMuscleGain = $calculator->caloriesForMuscleGain();
+$disclaimer = $calculator->getDisclaimer();
+
+include("../views/partials/header.php");
+
+include("../views/calories_result.html");
+
+
+include("../views/partials/footer.php");
 ?>
